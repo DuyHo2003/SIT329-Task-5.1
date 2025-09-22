0c0dbf4c5c7580c6587fa1c9154d3b0d57986840
DuyHo2003 <120005728+DuyHo2003@users.noreply.github.com>
2025-09-22 01:12:22 +0000

    Update TIMER_DEAKIN.h

--- TIMER_DEAKIN.h
+++ TIMER_DEAKIN.h
@@ -1,15 +1,15 @@
 #ifndef TIMER_DEAKIN_H
 #define TIMER_DEAKIN_H
 
-#include <stdbool.h>  // <-- Add this
+#include <stdbool.h>
 #include "sam.h"
 
 // Flags
-extern volatile bool blinkFlag; // Mode1
-extern volatile bool countFlag; // Mode2 & 3
+extern volatile bool blinkFlag;   // Mode 1 (Knight Rider)
+extern volatile bool countFlag;   // Mode 2 & 3
 
 // Timer init functions
 void TCC0_init(uint32_t period_ms);
-void TC5_init(uint32_t period_ms);
+void TC3_init(uint32_t period_ms);
 
 #endif
